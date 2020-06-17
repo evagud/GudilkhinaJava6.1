e9f8a2c2ffb8d57737d7f01c97abd612db2bff6d
Evgenia Gudilkhina <evagud22.4@gmail.com>
2020-06-17 11:06:20 +0400

    modified:StatsService.java,StatsServiceTest.java

--- GudilkhinaJava6.1/src/main/java/ru/netology/stats/StatsService.java
+++ GudilkhinaJava6.1/src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,86 @@
 package ru.netology.stats;
 
-public class StatsService {
+import java.util.stream.Stream;
+
+public class StatsService<count> {
+    public long calculateSum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+
+        return sum;
+    }
+
+    public long averageSelling(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        long aver = sum / 12;
+        return aver;
+    }
+
+    public long monthMaxSales(long[] sales) {
+        long currentMax = sales[0];
+        int indexMax = 1;
+        for (int i = 1; i < sales.length; i++) {
+            long sale = sales[i];
+            if (currentMax < sale) {
+                currentMax = sale;
+                indexMax = i+1;
+            }
+        }
+        return indexMax;
+
+    }
+
+    public long monthMinSales(long[] sales) {
+        long min = sales[0];
+        int indexForMin = 1;
+        for (int i = 1; i < sales.length; i++) {
+            long sale = sales[i];
+            if (min > sale) {
+                min = sale;
+                indexForMin = i+1;
+            }
+        }
+        return indexForMin;
+    }
+
+    public long quantityMonthMinSales(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        long aver = sum / 12;
+        int count = 0;
+        for (int i = 0; i < sales.length; i++) {
+            long sale = sales[i];
+            if (aver > sale)
+                count++;
+        }
+        return count;
+        }
+
+    public long quantityMonthMaxSales(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        long aver = sum / 12;
+        int count = 0;
+        for (int i = 0; i < sales.length; i++) {
+            long sale = sales[i];
+            if (aver < sale)
+                count++;
+        }
+        return count;
+
+    }
 }
+
+
+
+
+
--- GudilkhinaJava6.1/src/test/java/ru/netology/stats/StatsServiceTest.java
+++ GudilkhinaJava6.1/src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,5 +1,63 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
 
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//    {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}
+    @Test
+    void calculateSum() {
+        StatsService service = new StatsService();
+        long expected = 180;
+        long actual = service.calculateSum(sales);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void averageSelling() {
+        StatsService service = new StatsService();
+        long expected = 15;
+        long actual = service.averageSelling(sales);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void monthMaxSales() {
+        StatsService service = new StatsService();
+        long expected = 6;
+        long actual = service.monthMaxSales(sales);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void monthMinSales() {
+        StatsService service = new StatsService();
+        long expected = 9;
+        long actual = service.monthMinSales(sales);
+        assertEquals(expected,actual);
+    }
+
+
+//
+    @Test
+    void quantityMonthMinSales() {
+        StatsService service = new StatsService();
+        long expected = 5 ;
+        long actual = service.quantityMonthMinSales(sales);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void quantityMonthMaxSales(){
+        StatsService service = new StatsService();
+        long expected = 5;
+        long actual = service.quantityMonthMaxSales(sales);
+        assertEquals(expected,actual);
+    }
 }
